1f1c5777e695a11bcc2710bf2ec1ce8297449c56
erik-perez-1 <erikp2913@gmail.com>
2026-02-13 16:56:39 -0600

    feat: creacion del objeto HashMap junto con las 20 palabras

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        HashMap <String, String> diccionario= new HashMap<>();
+
+        diccionario.put("Scope", "Alcance");
+        diccionario.put("Keyword", "Palabra reservada");
+        diccionario.put("Loop", "Bucle");
+        diccionario.put("Array", "Arreglo");
+        diccionario.put("Thread", "Hilo de ejecucion");
+        diccionario.put("String", "Cadena de texto");
+        diccionario.put("Boolean", "Booleano");
+        diccionario.put("Method", "Metodo");
+        diccionario.put("Inheritance", "Herencia");
+        diccionario.put("Encapsulation", "Encapsulamiento");
+        diccionario.put("Polymorphism", "Polimorfismo");
+        diccionario.put("Constructor", "Constructor");
+        diccionario.put("Interface", "Interfaz");
+        diccionario.put("Package", "Paquete");
+        diccionario.put("Exception", "Excepcion");
+        diccionario.put("Null", "Nulo");
+        diccionario.put("Object", "Objeto");
+        diccionario.put("Class", "Clase");
+        diccionario.put("Key", "Clave");
+        diccionario.put("Value", "Valor");
+
+       // System.out.println("Estas liston para la prueba de fuego del programador?");
+    }
+}
